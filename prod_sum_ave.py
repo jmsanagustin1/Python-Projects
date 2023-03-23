2aaf8ee8c9cf557a8d3ad47add72abf39883036f
jmsanagustin1 <91039356+jmsanagustin1@users.noreply.github.com>
2023-03-23 10:36:39 +0800

    Create prod_sum_ave.py

---
+++ prod_sum_ave.py
@@ -0,0 +1,15 @@
+def jm():
+    n1=int(input("Enter First Number: "))
+    n2=int(input("Enter Second Number: "))
+    n3=int(input("Enter Third Number: "))
+
+    sum=n1+n2+n3
+    prod=n1*n2*n3
+    ave=sum/3
+    
+    print()
+    print("Sum: ", sum)
+    print("Product: ", prod)
+    print("Average: ", ave)
+
+jm()
